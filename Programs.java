7c5f1982137f9bc7146ccdbc6425ee52453ff9ae
Pulivarthi ParipurnaChari <86514078+Paripurnachari284858@users.noreply.github.com>
2026-01-08 20:28:59 +0530

    Programs.java

--- Programs.java
+++ Programs.java
@@ -1,11 +1,14 @@
+//this program to find length of number in one line
 import java.util.Scanner;
 public class Programs
 {
    public static void main(String args[]){
-
+//reading data from keybord
   Scanner scanner_obj=new Scanner(System.in);
   int number=scanner_obj.nextInt();
+      //converting number into String and using length() we can get length of number 
   int number_length=Integer.toString(number).length();
   System.out.printf("Length of %d is : %d",number,number_length);
 }
+
 }
